8203bcb360cb96d9d13530c2ce44e55070bfd8a9
Thomas Kudin <89225766+Schattenoff@users.noreply.github.com>
2025-05-03 22:44:33 +0300

    add vercel

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vercel()
   ],
   server: {
     host: true,
